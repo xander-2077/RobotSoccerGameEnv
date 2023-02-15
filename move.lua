c1b05df260951bb28730651e97f9716585ce384d
xander <1073692867@qq.com>
2023-02-15 15:30:50 +0800

    update

---
+++ move.lua
@@ -0,0 +1,4 @@
+simRobomaster.set_target_twist(0, {x=0.2, y=-0.1, theta=0.1});
+simRobomaster.set_target_twist(1, {y=0.2, theta=-0.1});
+simRobomaster.set_target_twist(2, {x=0.2, y=-0.12});
+simRobomaster.set_target_twist(3, {x=0.2, y=0.1})
